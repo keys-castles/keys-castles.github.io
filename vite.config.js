9e18336d41a09f66068600fa13ca49016b6c4be1
Josue Chavez <josuechavez1022@gmail.com>
2026-05-30 00:48:02 -0700

    moved mountain to public directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/keys-castles.github.io/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        boids: resolve(__dirname, 'Boids.html'), // Replace with your static HTML file name
+      },
+    },
+  },
 });
